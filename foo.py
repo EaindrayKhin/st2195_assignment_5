422ede812969363f927ce6fcdecfede6d5d76e59
EaindrayKhin <eaindraykhin9700@gmail.com>
2021-11-22 14:50:55 +0630

    added debugging_Python

---
+++ foo.py
@@ -0,0 +1,19 @@
+def is_divisible_by_k(x, k): 
+    '''
+Checks whether x is divisible by k.
+    '''
+    return x%k == 0
+
+'''
+Store all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding doubles)
+'''
+nums = []
+for i in range(1,1001):
+    if (is_divisible_by_k(i, 2) or is_divisible_by_k(i, 5)) or is_divisible_by_k(i, 7): 
+        nums.append(i)
+        
+'''
+Sum all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding doubles)
+'''
+print(nums)
+print(sum(nums))
